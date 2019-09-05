63e1013fd4751b44b12ef57045bed3bb04df4263
Adrian Labbé <adrilabbelol@gmail.com>
2019-09-05 19:17:11 -0400

    Remove debug code

--- Edidown/View controllers/DocumentBrowserViewController.swift
+++ Edidown/View controllers/DocumentBrowserViewController.swift
@@ -24,7 +24,7 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
     }
     
     /// A document to open when the view appears.
-    var documentURL: URL? = Bundle.main.url(forResource: "Templates/Markdown/Example/Untitled", withExtension: "md")
+    var documentURL: URL?
     
     // MARK: - Document browser view controller
     
